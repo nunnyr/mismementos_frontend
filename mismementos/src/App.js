c79f54fd96bed3338240e2f3fca59386c25288b9
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-10-19 18:36:44 -0400

    adds map to app

--- mismementos/src/App.js
+++ mismementos/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl'
 import './App.css';
 
 function App() {
+  const TOKEN ="pk.eyJ1Ijoia3NvbG9tb243IiwiYSI6ImNrZ2gzODEzdjA0OHkycWxmOXpkY2VhdDIifQ.lnW9qKBPgt5JkWPhEGml_A"
+
+  const [viewport, setViewPort]= useState({ 
+    latitude: 40.730610,
+    longitude: -73.935242,
+    width: "100vw",
+    height: "100vh",
+    zoom:10
+  });
+
   return (
-     <div>
-      <h1>Map</h1>
-     </div>
+    <div>
+     <ReactMapGL 
+     {...viewport} 
+     mapboxApiAccessToken={TOKEN}
+     onViewportChange={viewport=>{
+       setViewPort(viewport)
+     }}>
+     </ReactMapGL>
+    </div>
   );
 }
 
