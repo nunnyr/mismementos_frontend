d1c9eff3b913f98a22917092b6c918cc2689c162
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-10-20 09:56:45 -0400

    adds map component

--- mismementos/src/App.js
+++ mismementos/src/App.js
@@ -1,39 +1,20 @@
-import React, {useState} from 'react';
-import ReactMapGL from 'react-map-gl';
+import React from 'react';
+import Map from './Map'
 import MemoryForm from './MemoryForm'
 import './App.css';
 
-function App() {
-  const TOKEN ="pk.eyJ1Ijoia3NvbG9tb243IiwiYSI6ImNrZ2gzODEzdjA0OHkycWxmOXpkY2VhdDIifQ.lnW9qKBPgt5JkWPhEGml_A"
-
-  const [viewport, setViewPort]= useState({ 
-    latitude: 40.730610,
-    longitude: -73.935242,
-    width: "100vw",
-    height: "100vh",
-    zoom:10
-  });
-
-//create a handleChange
-  let handleChange=(viewport)=>{
-    setViewPort(viewport)
+class App extends React.Component {
+ 
+  render(){
+    return (
+      <div>
+         <MemoryForm/>
+         <Map/>
+      </div>
+    ) 
   }
 
-//adding a 
 
-
-  return (
-    <div>
-     <MemoryForm/>
-     <ReactMapGL 
-     {...viewport} 
-     mapboxApiAccessToken={TOKEN}
-     mapstyle= "mapbox://styles/ksolomon7/ckgh6bpp7060519pjac0q1pe6"
-     onViewportChange={handleChange}
-    >
-     </ReactMapGL>
-    </div>
-  );
 }
 
 export default App;
---
+++ mismementos/src/Map.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+function Map() {
+    const TOKEN ="pk.eyJ1Ijoia3NvbG9tb243IiwiYSI6ImNrZ2gzODEzdjA0OHkycWxmOXpkY2VhdDIifQ.lnW9qKBPgt5JkWPhEGml_A"
+  
+    const [viewport, setViewPort]= useState({ 
+      latitude: 40.730610,
+      longitude: -73.935242,
+      width: "100vw",
+      height: "100vh",
+      zoom:10
+    });
+  
+  //create a handleChange
+    let handleChange=(viewport)=>{
+      setViewPort(viewport)
+    }
+  
+  //adding a 
+  
+  
+    return (
+      <div>
+       <ReactMapGL 
+       {...viewport} 
+       mapboxApiAccessToken={TOKEN}
+       mapstyle= "mapbox://styles/ksolomon7/ckgh6bpp7060519pjac0q1pe6"
+       onViewportChange={handleChange}
+      >
+       </ReactMapGL>
+      </div>
+    );
+  }
+  
+  export default Map;
+  
